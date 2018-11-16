ab964b18b3badf84ff54138e17b5a7fd41584c49
Patrick Martin <patrick@hellotech.com>
2018-11-15 17:37:52 -0800

    Social Media Cont...

--- wp-content/themes/gia/single.php
+++ wp-content/themes/gia/single.php
@@ -71,9 +71,9 @@
             <div class="post-options-item share-post">
               <img src="<?php bloginfo('template_url'); ?>/_assets/images/icon-social-sharing.svg"/>
               <a href="#">Share this Post</a>
-              <?php echo DISPLAY_ULTIMATE_PLUS(); ?>
+              <?php //echo DISPLAY_ULTIMATE_PLUS(); ?>
               <!-- Load Facebook SDK for JavaScript -->
-              <!-- <div id="fb-root"></div>
+              <div id="fb-root"></div>
               <script>(function(d, s, id) {
                   var js, fjs = d.getElementsByTagName(s)[0];
                   if (d.getElementById(id)) return;
@@ -81,13 +81,24 @@
                   js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
                   fjs.parentNode.insertBefore(js, fjs);
                 }(document, 'script', 'facebook-jssdk'));
-              </script> -->
+              </script>
 
               <!-- Your share button code -->
-              <!-- <div class="fb-share-button"
+              <div class="fb-share-button"
                 data-href="<?php echo the_permalink(); ?>"
                 data-layout="button_count">
-              </div> -->
+              </div>
+              <style>
+              #fb-root{
+                display: none;
+              }
+              .fb_iframe_widget{
+                margin-left: 5px;
+              }
+              .fb_iframe_widget span{
+                vertical-align: middle!important;
+              }
+              </style>
             </div>
             <div class="post-options-item bookmark-page">
               <img src="<?php bloginfo('template_url'); ?>/_assets/images/icon-bookmark.svg"/>
